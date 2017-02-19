daecceafdd885f38576d573b055accfb47a996f5
mcasciato <michael.casciato@icloud.com>
2017-02-19 16:40:44 -0500

    created angular module and referenced it via ng-app

---
+++ app/assets/javascripts/app.js
@@ -0,0 +1,10 @@
+(function () {
+    'use strict';
+
+    angular
+        .module('mysite', ['ui.router', 'templates'])
+        .config(function($httpProvider) {
+        // for CSRF errors
+        $httpProvider.defaults.headers.common['X-CSRF-Token'] = $('meta[name=csrf-token]').attr('content');
+    });
+}());
