67717acd9e5f9fdbe12a39817bdf19d0bfee0be1
mcasciato <michael.casciato@icloud.com>
2017-02-19 16:55:32 -0500

    added routes for other pages

--- app/assets/javascripts/routes.js
+++ app/assets/javascripts/routes.js
@@ -9,6 +9,19 @@
                     url: '/',
                     templateUrl: 'home/home.html'
                 })
+                .state('home.about', {
+                    url: 'about',
+                    templateUrl: 'about/about.html'
+                })
+                .state('home.portfolio', {
+                    url: 'portfolio',
+                    templateUrl: 'portfolio/portfolio.html'
+                })
+                .state('home.contact', {
+                    url: 'contact',
+                    templateUrl: 'contact/contact.html'
+                })
+
             $urlRouterProvider.otherwise('/')
         });
 }());
