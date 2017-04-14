66404e40b50784eb9a84e1f5110a96709a3cb1db
mcasciato <michael.casciato@icloud.com>
2017-04-13 20:38:59 -0400

    routes.js - fixed dependency injection, placed in IIFE

--- app/assets/javascripts/routes.js
+++ app/assets/javascripts/routes.js
@@ -1,39 +1,45 @@
-angular
-    .module('app')
-    .config(function ($stateProvider, $urlRouterProvider) {
-        $stateProvider
-            .state('home', {
-                url: '/',
-                templateUrl: 'home/home.html'
-            })
-            .state('home.skills', {
-                url: 'skills',
-                templateUrl: 'skills/skills.html'
-            })
-            .state('home.portfolio', {
-                url: 'portfolio',
-                templateUrl: 'portfolio/portfolio.html'
-            })
-            .state('home.flix4me', {
-                url: 'portfolio/flix4me',
-                templateUrl: 'portfolio/flix4me.html'
-            })
-            .state('home.myceus', {
-                url: 'portfolio/myceus',
-                templateUrl: 'portfolio/myceus.html'
-            })
-            .state('home.sinatraInstruments', {
-                url: 'portfolio/sinatraInstruments',
-                templateUrl: 'portfolio/sinatraInstruments.html'
-            })
-            .state('home.politicalNews', {
-                url: 'portfolio/politicalNews',
-                templateUrl: 'portfolio/politicalNews.html'
-            })
-            .state('home.contact', {
-                url: 'contact',
-                templateUrl: 'contact/contact.html'
-            })
+(function () {
 
-        $urlRouterProvider.otherwise('/')
-    });
+    'use strict';
+
+    angular
+        .module('app')
+        .config(['$stateProvider', '$urlRouterProvider', function($stateProvider, $urlRouterProvider) {
+            $stateProvider
+                .state('home', {
+                    url: '/',
+                    templateUrl: 'home/home.html'
+                })
+                .state('home.skills', {
+                    url: 'skills',
+                    templateUrl: 'skills/skills.html'
+                })
+                .state('home.portfolio', {
+                    url: 'portfolio',
+                    templateUrl: 'portfolio/portfolio.html'
+                })
+                .state('home.flix4me', {
+                    url: 'portfolio/flix4me',
+                    templateUrl: 'portfolio/flix4me.html'
+                })
+                .state('home.myceus', {
+                    url: 'portfolio/myceus',
+                    templateUrl: 'portfolio/myceus.html'
+                })
+                .state('home.sinatraInstruments', {
+                    url: 'portfolio/sinatraInstruments',
+                    templateUrl: 'portfolio/sinatraInstruments.html'
+                })
+                .state('home.politicalNews', {
+                    url: 'portfolio/politicalNews',
+                    templateUrl: 'portfolio/politicalNews.html'
+                })
+                .state('home.contact', {
+                    url: 'contact',
+                    templateUrl: 'contact/contact.html'
+                })
+
+            $urlRouterProvider.otherwise('/')
+        }]);
+
+}());
